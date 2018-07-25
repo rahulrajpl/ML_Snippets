c78b1f839f8a03f0980004fc69fd855b00bbfe69
rahulrajpl <lukrahul@gmail.com>
2018-07-26 01:23:40 +0530

    Take a look at the data a few different ways:
        (a) Dimensions of the dataset.
        (b) Peek at the data itself.
        (c) Statistical summary of all attributes.
        (d) Breakdown of the data by the class variable.

--- Project 1 - Iris Flower Classification/dataset_summary.py
+++ Project 1 - Iris Flower Classification/dataset_summary.py
@@ -17,7 +17,7 @@ url = "/home/parvathy/PycharmProjects/ML_Snippets/Project 1 - Iris Flower Classi
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
 
-# print(dataset.shape)
-# print(dataset.head(20))
-# print(dataset.describe())
+print(dataset.shape)
+print(dataset.head(20))
+print(dataset.describe())
 print(dataset.groupby('class').size())

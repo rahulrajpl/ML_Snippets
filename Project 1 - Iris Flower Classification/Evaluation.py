aa2e7ebb2f39fc56b818431e9a33b0c6423f5e4a
rahulrajpl <lukrahul@gmail.com>
2018-07-28 23:20:46 +0530

    ScatterPlot of interaction between variables

---
+++ Project 1 - Iris Flower Classification/Evaluation.py
@@ -0,0 +1,33 @@
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+url = "/home/parvathy/PycharmProjects/ML_Snippets/Project 1 - Iris Flower Classification/iris.data"
+
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+# box and whisker plots
+# This is an unvariate plot, means plots of each individual variable
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# We can also create a histogram of each input variable to get an idea of the distribution.
+dataset.hist()
+plt.show()
+
+# Now we can look at the interactions between the variables.
+# First, let’s look at scatterplots of all pairs of attributes. This can be helpful to spot structured relationships between input variables.
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()

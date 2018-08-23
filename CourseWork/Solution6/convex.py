32dbec3ce677dfb4c1620e156c743bfbae7812e0
rahulrajpl <lukrahul@gmail.com>
2018-08-24 01:39:42 +0530

    Calculated Euclidean Distance of vectors and got he accuracy of 47.8 % in prediction

--- CourseWork/Solution6/convex.py
+++ CourseWork/Solution6/convex.py
@@ -17,49 +17,47 @@ for c in range(10):
     sim_vector.append(sim_c)
 sim_vector = np.asarray(sim_vector)
 
-# Normalising the similarity Vector
+# # Normalising the similarity Vector
 for s in sim_vector:
     sim_c_normalised = np.asarray([s[k]/sum(s) for k in range(40)])
     # sim_c_normalised = np.asarray([s[k]/sum(s) for k in range(40)])
     similarity_vector.append(sim_c_normalised)
 similarity_vector = np.asarray(similarity_vector)
-
-
+# print(similarity_vector.shape)
 
 # Calculating mean of each seen class
 # mean_of_seen_classes = np.asarray([X_seen[i].mean() for i in range(40)])
-mean_of_seen_classes = np.asarray([np.reshape(X_seen[i], X_seen[i].shape[0]*4096).mean() for i in range(40)])
+mean_seen = np.asarray([np.mean(X_seen[i], axis=0) for i in range(40)])
+# print(mean_seen.shape)
 
 # Finding Mean of unseen classes
-mean_of_unseen_classes = []
-for s in similarity_vector:
-    mean_of_unseen_classes.append(sum([s[k]*mean_of_seen_classes[k] for k in range(40)]))
-
-for i in mean_of_unseen_classes:
-    print(i)
-# Combining all 50 nos means calculated in to "mean_of_classes"
-mean_of_classes =  mean_of_unseen_classes
+mean_unseen = np.matmul(similarity_vector, mean_seen)
 
 
 # Running Test Data and Predicting the class
-# m_test = X_test[1].mean()
-# print("Test input is ", m_test)
-# m_predicted = min(mean_of_classes, key=lambda x:abs(x-m_test))
-# print("Predicted input is", m_predicted)
-# m_class = mean_of_classes.index(min(mean_of_classes, key=lambda x:abs(x-m_test)))
-# # print("Predicted Class is ", mean_of_classes.index(m_predicted))
-# print("Predicted Class is ", m_class+1)
-# print("Actual Class is ", int(Y_test[1]))
+predicted_class = []
+correct = 0
 
-correct_predictions = 0
-for t,y in zip(X_test,Y_test):
-    # m_test = t.mean()
-    m_test = np.reshape(t, t.shape[0]).mean()
-    m_predicted = min(mean_of_classes, key=lambda x: abs(x - m_test))
-    m_class = mean_of_classes.index(m_predicted)+1
-    if m_class == int(y[0]):
-        correct_predictions += 1
+for t in range(len(X_test)):
+    dist = [np.linalg.norm(mean_unseen[i]-X_test[t]) for i in range(10)]
+    predicted_class.append(1+dist.index(min(dist)))
+    # print("predicted = ", 1+dist.index(min(dist)), " and Truth is", int(Y_test[t]))
+    if 1+dist.index(min(dist)) == int(Y_test[t]):
+        correct += 1
+print(correct, "correct out of ", len(X_test))
+print("Accuracy = ", correct/len(X_test))
 
-print(correct_predictions)
-for t in X_test:
-    print(t.shape)
+# dist = []
+#
+# correct = 0
+# for test in X_test:
+#     for i in range(10):
+#         d = np.linalg.norm(test- mean_unseen[i])
+#         dist.append(d)
+#     predicted_class.append(dist.index(min(dist))+1)
+#
+# for i in range(len(Y_test)):
+#     if predicted_class[i]== int(Y_test[0]):
+#         correct += 1
+#
+# print(correct)

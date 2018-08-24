f14ac7faeb86ec8ccd01a09c2ac7ec411d35a4cd
rahulrajpl <lukrahul@gmail.com>
2018-08-24 07:54:04 +0530

    Calculated Euclidean Distance of vectors and got he accuracy of 47.8 % in prediction

--- CourseWork/Solution6/convex.py
+++ CourseWork/Solution6/convex.py
@@ -9,10 +9,8 @@ Y_test = np.load('Ytest.npy', encoding='latin1')
 
 # Finding the similarity vector of unseen class through convex combination
 sim_vector = []
-# sim_c = np.empty(shape=[1,40])
 similarity_vector = []
 for c in range(10):
-    # sim_c = np.asarray([np.dot(class_attributes_unseen[c], class_attributes_seen[k]) for k in range(40)])
     sim_c = [np.dot(class_attributes_unseen[c], class_attributes_seen[k]) for k in range(40)]
     sim_vector.append(sim_c)
 sim_vector = np.asarray(sim_vector)
@@ -20,15 +18,11 @@ sim_vector = np.asarray(sim_vector)
 # # Normalising the similarity Vector
 for s in sim_vector:
     sim_c_normalised = np.asarray([s[k]/sum(s) for k in range(40)])
-    # sim_c_normalised = np.asarray([s[k]/sum(s) for k in range(40)])
     similarity_vector.append(sim_c_normalised)
 similarity_vector = np.asarray(similarity_vector)
-# print(similarity_vector.shape)
 
 # Calculating mean of each seen class
-# mean_of_seen_classes = np.asarray([X_seen[i].mean() for i in range(40)])
 mean_seen = np.asarray([np.mean(X_seen[i], axis=0) for i in range(40)])
-# print(mean_seen.shape)
 
 # Finding Mean of unseen classes
 mean_unseen = np.matmul(similarity_vector, mean_seen)
@@ -40,24 +34,9 @@ correct = 0
 
 for t in range(len(X_test)):
     dist = [np.linalg.norm(mean_unseen[i]-X_test[t]) for i in range(10)]
-    predicted_class.append(1+dist.index(min(dist)))
-    # print("predicted = ", 1+dist.index(min(dist)), " and Truth is", int(Y_test[t]))
-    if 1+dist.index(min(dist)) == int(Y_test[t]):
+    p_class = 1+dist.index(min(dist))
+    predicted_class.append(p_class)
+    if p_class == int(Y_test[t]):
         correct += 1
 print(correct, "correct out of ", len(X_test))
-print("Accuracy = ", correct/len(X_test))
-
-# dist = []
-#
-# correct = 0
-# for test in X_test:
-#     for i in range(10):
-#         d = np.linalg.norm(test- mean_unseen[i])
-#         dist.append(d)
-#     predicted_class.append(dist.index(min(dist))+1)
-#
-# for i in range(len(Y_test)):
-#     if predicted_class[i]== int(Y_test[0]):
-#         correct += 1
-#
-# print(correct)
+print("Accuracy = ", correct/len(X_test) * 100, "%")

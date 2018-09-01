72ddf8bcde65600713dc27a1f2f6e4be4eeb9ed9
rahulrajpl <lukrahul@gmail.com>
2018-09-01 22:09:31 +0530

    SW gdp cleaned. Missing values managed

--- Data Mining/Handling_Missing_Values.py
+++ Data Mining/Handling_Missing_Values.py
@@ -8,7 +8,7 @@ r = pd.read_csv("regions.csv")
 gdp_const = pd.read_csv("./datagov/Economy/gross-domestic-product-gdp-constant-price.csv")
 gdp_curr = pd.read_csv("./datagov/Economy/gross-domestic-product-gdp-current-price.csv")
 sw_gdp_const = pd.read_csv("./datagov/Economy/state-wise-net-domestic-product-ndp-constant-price.csv")
-# sw_gdp_curr = pd.read_csv("./datagov/Economy/state-wise-net-domestic-product-ndp-current-price.csv")
+sw_gdp_curr = pd.read_csv("./datagov/Economy/state-wise-net-domestic-product-ndp-current-price.csv")
 # dor = pd.read_csv("./datagov/Education/drop-out-rate.csv")
 # ger_he = pd.read_csv("./datagov/Education/gross-enrolment-ratio-higher-education.csv")
 # ger_schools = pd.read_csv("./datagov/Education/gross-enrolment-ratio-schools.csv")
@@ -25,7 +25,7 @@ gdp_curr.drop(columns=['Items  Description'], inplace = True, axis=1)
 sw_gdp_const.drop(columns=['Item Description'], inplace = True)
 sw_gdp_const.drop(index=11, inplace=True)
 
-# Correcting State names
+# Correcting State names in dataframes
 gdp_const = gdp_const.rename(columns={'Andhra Pradesh ': 'Andhra Pradesh',
                         'West Bengal1': 'West Bengal',
                        'Andaman & Nicobar Islands': 'A & N Islands',
@@ -36,6 +36,12 @@ gdp_curr = gdp_curr.rename(columns={'Andhra Pradesh ': 'Andhra Pradesh',
                        'Andaman & Nicobar Islands': 'A & N Islands',
                        'Delhi': 'NCT of Delhi'
                        })
+sw_gdp_const =sw_gdp_const.rename(columns={'Andhra Pradesh ': 'Andhra Pradesh',
+                        'West Bengal1': 'West Bengal',
+                       'Andaman & Nicobar Islands': 'A & N Islands',
+                       'Delhi': 'NCT of Delhi'
+                       })
+
 rwise = r.groupby('Region')['States and Union Territories'].apply(list).to_dict()
 # Grouping the States in the GDP Dataframe
 rwise_states = []
@@ -52,8 +58,9 @@ gdp_const.fillna(method='ffill', inplace=True)
 # Saving final cleaned file to output folder
 gdp_const.to_csv("./output/Economy/gross-domestic-product-gdp-constant-price.csv")
 print("GDP_const cleaned and saved to output folder")
-#---------
 
+#---------
+#Cleaning gdp_curr Start
 for reg in dr.groups.keys():
     states = [i for i in dr.get_group(reg)['States and Union Territories'] if i in gdp_curr.columns]
     rwise_states.append(states)
@@ -65,8 +72,18 @@ for rstate in rwise_states:
 gdp_curr.fillna(method='ffill', inplace=True)
 gdp_const.to_csv("./output/Economy/gross-domestic-product-gdp-current-price.csv")
 print("GDP_curr cleaned and saved to output folder")
-#---------
 
+#---------
+# Cleaning sq_gdp Start
+for reg in dr.groups.keys():
+    states = [i for i in dr.get_group(reg)['States and Union Territories'] if i in sw_gdp_const.columns]
+    rwise_states.append(states)
+# Calculating the mean of nan region wise.
+for rstate in rwise_states:
+    sw_gdp_const[rstate] = sw_gdp_const[rstate].apply(lambda row: row.fillna(row.mean()), axis=1)
+sw_gdp_const.to_csv("./output/Economy/state-wise-net-domestic-product-ndp-constant-price.csv")
+print("sw_gdp_const cleaned and saved to output folder")
+#---------
 
 
 

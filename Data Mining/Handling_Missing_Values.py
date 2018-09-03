31665a3c475ca53f3524279fdb27dc1c5f555424
rahulrajpl <lukrahul@gmail.com>
2018-09-03 22:59:26 +0530

    Minor Errors in saving output corrected

--- Data Mining/Handling_Missing_Values.py
+++ Data Mining/Handling_Missing_Values.py
@@ -63,6 +63,7 @@ for rstate in rwise_states:
 # Last year's data of same state being filled as an estimate.
 gdp_const.fillna(method='ffill', inplace=True)
 # Saving final cleaned file to output folder
+gdp_const.rename(columns={'All_India GDP':'INDIA'},inplace=True)
 gdp_const.to_csv("./output/Economy/gross-domestic-product-gdp-constant-price.csv", index=False)
 print("GDP_const cleaned and saved to output folder")
 
@@ -78,7 +79,9 @@ for rstate in rwise_states:
 # Those regional states where all values are NaN. Mean cannot be taken. So used ffill used.
 # Last year's data of same state being filled as an estimate.
 gdp_curr.fillna(method='ffill', inplace=True)
-gdp_const.to_csv("./output/Economy/gross-domestic-product-gdp-current-price.csv", index=False)
+gdp_curr.rename(columns={'All_India GDP':'INDIA'},inplace=True)
+
+gdp_curr.to_csv("./output/Economy/gross-domestic-product-gdp-current-price.csv", index=False)
 print("GDP_curr cleaned and saved to output folder")
 #---------
 
@@ -90,6 +93,9 @@ for reg in dr.groups.keys():
 # Calculating the mean of nan region wise.
 for rstate in rwise_states:
     sw_gdp_const[rstate] = sw_gdp_const[rstate].apply(lambda row: row.fillna(row.mean()), axis=1)
+
+
+sw_gdp_const.rename(columns={'All_India NDP':'INDIA'},inplace=True)
 sw_gdp_const.to_csv("./output/Economy/state-wise-net-domestic-product-ndp-constant-price.csv", index=False)
 print("sw_gdp_const cleaned and saved to output folder")
 #---------
@@ -103,6 +109,7 @@ for reg in dr.groups.keys():
 for rstate in rwise_states:
     sw_gdp_curr[rstate] = sw_gdp_curr[rstate].apply(lambda row: row.fillna(row.mean()), axis=1)
 sw_gdp_curr.fillna(method='ffill', inplace=True)
+sw_gdp_curr.rename(columns={'All_India NDP':'INDIA'},inplace=True)
 sw_gdp_curr.to_csv("./output/Economy/state-wise-net-domestic-product-ndp-current-price.csv", index=False)
 print("sw_gdp_curr cleaned and saved to output folder")
 #-----------
